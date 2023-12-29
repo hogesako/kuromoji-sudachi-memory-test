b14b2fd665cae8edf39f8175881d1104ee2c3577
hogesako <hogesako@ikasako.com>
2023-12-29 16:57:08 +0900

    insert index

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # kuromoji-sudachi-memory-test
+OpenSearchにおいて、sudachiとkuromojiの形態素解析におけるヒープメモリの利用量を比較したい
+
+## 検証の流れ
+1. OpenSearchのGCをEpsilonGCに変更して起動
+2. Wikipediaの文書を79250件(jawiki-latest-pages-articles1分)入れる
+3. ヒープダンプを取得してヒープメモリの状況確認
+
+## 検証バージョン
+* OpenSearch: 2.11.1
+* kuromoji: OpenSearch2.11.1に同梱のもの(lucene9.8.0)
+* elasticsearch-sudachiプラグイン: v3.1.0
+* sudachi辞書 :v20230927-core
---
+++ insertindex/insertindex.py
@@ -0,0 +1,40 @@
+import glob
+import json
+from opensearchpy import OpenSearch
+
+jsonl_files = glob.glob("jawiki.json/**/wiki_*")
+
+index_name = 'tokenize-memory-test'
+
+
+host = 'localhost'
+port = 9200
+
+client = OpenSearch(
+    hosts = [{'host': host, 'port': port}],
+    http_compress = True,
+    use_ssl = False,
+    verify_certs = False,
+    ssl_assert_hostname = False,
+    ssl_show_warn = False
+)
+
+for jsonl_file in jsonl_files:
+    with open(jsonl_file, 'r') as file:
+        for line in file:
+            try:
+                wikiObj = json.loads(line)
+                document = {
+                    'title': wikiObj['title'],
+                    'text': wikiObj['text'],
+                }
+                response = client.index(
+                    index = index_name,
+                    body = document,
+                    id = id,
+                    refresh = False
+                )
+                print(wikiObj['title'])
+                id += 1
+            except json.JSONDecodeError:
+                print("invalid json")
